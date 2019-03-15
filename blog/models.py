3f2715fbf892e7d94bdcf29033ed48860c720021
LLL <81154420@qq.com>
2019-03-16 01:00:17 +0800

    updata 3.16

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
-# coding: utf-8
-
+    # coding: utf-8
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
 from django.utils.six import python_2_unicode_compatible
 
 
@@ -31,6 +32,11 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     def __str__(self):
         return self.title
@@ -40,3 +46,12 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['created_time', 'title']
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),#更改
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
+    path('', views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),#更改
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
 ]
